ceca948fefe8e853f8b9694ae1ab493754dd4da5
Lyz03 <lizoe@netcourrier.com>
2021-12-21 10:15:33 +0100

    exo 122

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
+const holes = $('.hole');
+//utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
+const scoreBoard = $('.score');
+// utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const moles = $('.mole');
+
+let i = 0;
+let interval;
+
+function randomHole() {
+    let randomNumber = Math.floor(Math.random() * holes.length);
+    return $('.hole' + randomNumber);
+}
+
+function randomTime() {
+    let randomNumber = Math.round(Math.random() * 1000);
+    if (randomNumber < 200)
+        return randomTime();
+    return randomNumber;
+}
+
+function runGame() {
+    if (!stopGame()) {
+       let curentHole = randomHole().addClass('up');
+        setTimeout(function () {
+            curentHole.removeClass('up');
+            runGame()
+        }, randomTime())
+    }
+}
+
+function stopGame() {
+    if (i === 10) {
+        clearInterval(interval)
+        return true
+    }
+}
+
+$('#startGame').click(function () {
+    interval = setInterval(function () {
+        i++
+        stopGame()
+    }, 1000)
+    runGame()
+});
+
+moles.click(function () {
+    console.log(scoreBoard.text() + 1)
+    scoreBoard.text((parseInt(scoreBoard.text()) + 1).toString());
+    moles.removeClass('up');
+    runGame();
+});
